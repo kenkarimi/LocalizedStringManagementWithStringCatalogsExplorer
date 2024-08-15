6134b56fa1bc0b962e35b0897d3c9b010a1516e4
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-08-15 18:50:18 +0300

    Initial Commit

---
+++ LocalizedStringManagementWithStringCatalogsExplorer/LocalizedStringManagementWithStringCatalogsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedStringManagementWithStringCatalogsExplorerApp.swift
+//  LocalizedStringManagementWithStringCatalogsExplorer
+//
+//  Created by Kennedy Karimi on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedStringManagementWithStringCatalogsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
